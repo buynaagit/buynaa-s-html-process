05d38d0e0e229a40e73dd6e99d7f518382580a12
buynaagit <68780094+buynaagit@users.noreply.github.com>
2020-10-12 00:54:24 +0800

    Create ih_baga.c

---
+++ ih_baga.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+ 
+int main() {
+   int a[30], i, num, smallest, biggest;
+
+ 
+   printf("\nEnter no of elements :");
+   scanf("%d", &num);
+ 
+   //Read n elements in an array
+   for (i = 0; i < num; i++)
+      scanf("%d", &a[i]);
+ 
+   //Consider first element as smallest
+ 
+  
+  smallest=1000;
+  for (i = 0; i < num; i++) {
+   	  if(a[i]>0)
+      if (a[i] < smallest) {
+         smallest = a[i];
+      }
+   }
+
+ 
+  biggest = -1000;
+  for (i = 0; i < num; i++) {
+       if(a[i]<0)
+      if (a[i] > biggest) {
+         biggest = a[i];
+      }
+     
+   }
+
+   
+ 
+   // Print out the Result
+   printf("\nhamgiin baga eyreg too : %d", smallest);
+   printf("\nhamgiin ih surug too: %d", biggest);
+ 
+   return (0);
+}
